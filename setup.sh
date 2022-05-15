e6caa61dd3185b8a0884270468d2ac14089a4289
Manvendra Upadhyay <ishaans019@protonmail.com>
2022-05-15 17:34:57 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"darekarabhishek@gmail.com\"\n\
+email = \"ishaans019@protonmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
