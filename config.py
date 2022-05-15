5a04d5a63bbca4ee73622a35313a3e0f5684d7c6
Manvendra Upadhyay <ishaans019@protonmail.com>
2022-05-15 17:29:45 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "Y5IF5SaC5bjbPAj1pCGpOjQLu"
+consumer_secret = "BfdfxV45X1nOsubcwQObZztilxRpBFqtzruCm4zlWfRjyrYdpo"
+access_token = "1189177405308456960-ojj0BUyO3wIzEaPIit4mWPL7uHo9FW"
+access_token_secret = "v2VRC24Bi1zmxUldiymlVLQOj4o1eE9zfzJwNRLCXxDvU"
