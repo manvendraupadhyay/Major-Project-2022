31dcc243fa82b124e9827e6b06fb3590619f6952
Manvendra Upadhyay <ishaans019@protonmail.com>
2022-05-15 19:01:51 +0530

    Update App_Streamlit.py

--- App_Streamlit.py
+++ App_Streamlit.py
@@ -66,7 +66,7 @@ def main():
     def get_tweets(Topic, Count):
         i = 0
         # my_bar = st.progress(100) # To track progress of Extracted tweets
-        for tweet in tweepy.Cursor(api.search, q=Topic, count=100, lang="en", exclude='retweets').items():
+        for tweet in tweepy.Cursor(api.search, q=Topic, count=500, lang="en", exclude='retweets').items():
             # time.sleep(0.1)
             # my_bar.progress(i)
             df.loc[i, "Date"] = tweet.created_at
